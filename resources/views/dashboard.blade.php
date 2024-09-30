574828bc3289d266c7b94d3117cfde9d40a3f1b7
alex <reyvaldiviaalejandro@gmail.com>
2024-09-30 17:18:32 +0000

    controlers

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,12 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
+                    @foreach ($links as $link)
+                        <li>{{$link->title}}</li>
+                    @endforeach
+                    {{$links->links()}}
+
+                    <small>Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
                 </div>
             </div>
         </div>
